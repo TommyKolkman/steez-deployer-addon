38e89ef2f0670b1af936b0fa2b5a5b9f0ec5ef9b
Tommy Kolkman <info@steez.nl>
2022-08-19 15:14:03 +0200

    Add the configuration for building NPM production locally and cleaning it up

--- deploy-inc/npm.php
+++ deploy-inc/npm.php
@@ -1,55 +1,29 @@
 <?php
-/**
- * NOTICE OF LICENSE
+/* (c) Anton Medvedev <anton@medv.io>
  *
- * This source file is subject to the General Public License (GPL 3.0).
- * This license is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/gpl-3.0.en.php
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade this module to newer
- * versions in the future.
- *
- * @author      Tommy Kolkman <tommy@steez.nl>
- * @copyright   Copyright (c) 2020 Steez (https://steez.nl)
- * @license     http://opensource.org/licenses/gpl-3.0.en.php General Public License (GPL 3.0)
- *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-/**
- * Building assets
- * @todo This is possible, but very slow!
- */
-//desc( 'Build the assets on the server' );
-//task( 'build:assets:dev', 'cd {{ release_path }}/wp-content/themes/{{ theme_folder_path }} && npm install --loglevel info && npm run development --loglevel info && cd {{ release_path }}');
-use function Deployer\desc;
-use function Deployer\has;
-use function Deployer\run;
-use function Deployer\set;
-use function Deployer\task;
-use function Deployer\test;
+namespace Deployer;
 
 set('bin/npm', function () {
 	return run('which npm');
 });
 
-desc('Install npm packages');
-task('npm:install_custom', function () {
+
+task('npm:install', function () {
+	desc('Install npm packages');
 	if (has('previous_release')) {
-		if (test('[ -d {{previous_release}}/wp-content/themes/{{ theme_folder_path }}/node_modules ]')) {
-			run('cp -R {{previous_release}}/wp-content/themes/{{ theme_folder_path }}/node_modules {{release_path}}');
+		if (test('[ -d {{previous_release}}/node_modules ]')) {
+			run('cp -R {{previous_release}}/node_modules {{release_path}}');
 		}
 	}
-	run('node -v');
-	run('npm -v');
-	run("cd {{release_path}}/wp-content/themes/{{ theme_folder_path }} && {{bin/npm}} install");
+	run("cd {{release_path}}/wp-content/themes/{{theme_folder_path}} && {{bin/npm}} install");
 });
-
-task('npm:run_development', function () {
-	run("cd {{release_path}}/wp-content/themes/{{ theme_folder_path }} && {{bin/npm}} run development");
-});
-
-task('npm:run_production', function () {
-	run("cd {{release_path}}/wp-content/themes/{{ theme_folder_path }} && {{bin/npm}} run production");
+task('npm:production', function () {
+	desc('Build all the assets, remove node_modules and the assets afterwards');
+	run("cd {{release_path}}/wp-content/themes/{{theme_folder_path}} && {{bin/npm}} run production");
+	run("rm -rf {{release_path}}/wp-content/themes/{{theme_folder_path}}/node_modules");
+	run("rm -rf {{release_path}}/wp-content/themes/{{theme_folder_path}}/assets");
 });
